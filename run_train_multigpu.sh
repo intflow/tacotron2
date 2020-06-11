545a0d88b49709216ac29ce421bd65b6e21426c2
kmjeon <lordet01@gmail.com>
2020-06-11 22:20:14 +0900

    skylife train in progress

--- run_train_multigpu.sh
+++ run_train_multigpu.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 pkill python3
+pkill tensorboard
+
 rm -rf ./outdir/logdir/*
 #python3 -m multiproc train.py --output_directory=outdir --log_directory=logdir --hparams=distributed_run=True,fp16_run=True &
 #python3 -m multiproc train.py --output_directory=outdir --log_directory=logdir --hparams=distributed_run=True,fp16_run=True -c tacotron2_statedict.pt --warm_start&
 #python3 -m multiproc train.py --output_directory=outdir --log_directory=logdir --hparams=distributed_run=True,fp16_run=True -c model_zoo/tacotron2_kss2000.pt --warm_start&
-python3 -m multiproc train.py --output_directory=outdir --log_directory=logdir --hparams=distributed_run=True,fp16_run=True -c outdir/checkpoint_8000_skylife.pt --warm_start&
-tensorboard --logdir=outdir/logdir
+tensorboard --logdir=outdir/logdir &
+python3 -m multiproc train.py --output_directory=outdir --log_directory=logdir --hparams=distributed_run=True,fp16_run=True -c outdir/checkpoint_0 --warm_start
